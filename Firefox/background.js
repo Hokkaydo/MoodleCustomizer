562bee68edb26e3707aeaa85f7a5a322bca57961
Kaporos <theo@daron.be>
2023-12-09 09:04:13 +0100

    simple hook for recap.php

--- Firefox/background.js
+++ Firefox/background.js
@@ -1,17 +1,36 @@
 browser.webRequest.onBeforeRequest.addListener(
     async function (details) {
         let settings = await browser.storage.local.get("avoidPdfDownload");
-        console.log(settings)
-        if (!details.url.includes("moodle") || !details.url.includes("forcedownload=1") || settings.avoidPdfDownload === false) {
-          return;
+        if (!details.url.includes("moodle")) {
+            return;
         }
-        let new_url = details.url.replace("forcedownload=1","forcedownload=0");
-        return {
-            redirectUrl: new_url
+
+        if (details.url.endsWith("/my/recap.php")) {
+            //hook response.
+            let filter = browser.webRequest.filterResponseData(details.requestId);
+            filter.ondata = (e) => hook_recap(filter, details.url, e);
+            return;
         }
+
+        //pdf redirection
+        if (details.url.includes("forcedownload=1") && settings.avoidPdfDownload === true) {
+            let new_url = details.url.replace("forcedownload=1","forcedownload=0");
+            return {
+                redirectUrl: new_url
+            }
+        }
+
     },
     {
         urls: ["<all_urls>"]
     },
     ["blocking"]
 )
+
+async function hook_recap(filter, url, event) {
+    let anotherUrl = url.replace("recap.php","");
+    let response = await fetch(anotherUrl);
+    filter.write(new TextEncoder().encode(await response.text()));
+    filter.statusCode = 500
+    filter.disconnect();
+}
